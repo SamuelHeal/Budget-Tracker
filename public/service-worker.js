2a0b8f655e9eaec029a6353261e8a1e34ad11c8b
Samuel Heal <samuelheal@hotmail.com>
2021-07-12 14:01:07 +1000

    manifest completed, service-worker and indexedDB files added

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/indexedDB.js",
+  "/manifest.webmanifest",
+  "/service-worker.js",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+
+];
